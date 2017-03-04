33d338dcf19fa1b8ba13bb0d61c859a9770878ee
Nick Ackermann <nicka99lp@gmail.com>
2017-03-04 22:26:30 +0100

    remove old Service¨

--- services/ticketService/ticketService.go
+++
@@ -1,9 +0,0 @@
-package ticketService
-
-import "github.com/NiciiA/GoGraphQL/models/ticketModel"
-
-func GetAll() []ticketModel.Ticket {
-	var TicketList []ticketModel.Ticket
-	TicketList = append(TicketList, ticketModel.Ticket{ID: "1"}, ticketModel.Ticket{ID: "2"})
-	return TicketList
-}
